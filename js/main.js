6120352e7b4d03f55c7a2ce24b7931f7022115ae
Emanuele-Guerra <emaguerra1993@hotmail.it>
2023-10-27 16:28:04 +0200

    push3

--- js/main.js
+++ js/main.js
@@ -1,32 +1,36 @@
 let nome = document.getElementById("nome");
 let km = document.getElementById("kilometri");
-let minorenne = document.getElementById("minorenne");
-let standard = document.getElementById("standard");
-let senior = document.getElementById("senior");
+// let minorenne = document.getElementById("minorenne");
+// let standard = document.getElementById("standard");
+// let senior = document.getElementById("senior");
 
-let km = ("")
+let eta = document.getElementById("eta");
 
 let prezzoPerKm = 0.21
 
 let prezzoTotale = (km * prezzoPerKm);
+let genera = document.getElementById("genera");
+
+console.log("eta: ", eta);
 
 
 
-if (eta == senior ) {
+if (eta == "senior" && genera != null) {
 
-    (prezzoTotale *= 0.6)
+    (prezzoTotale *= 0.6);
+    document.getElementById("costo").innerHTML("");
 }
 
 
-else if (eta == minorenne) {
+else if (eta == "minorenne") {
 
-    (prezzoTotale *= 0.8)
+    (prezzoTotale *= 0.8);
 }
     
 
-else (eta == standard) {
+else if (eta == "standard") {
 
-    (prezzoPerKm * km) 
+    (prezzoPerKm * km) ;
 }
 
    
