c05e6bc81d374dd480217c14b16a21ec8c400cec
Emanuele-Guerra <emaguerra1993@hotmail.it>
2023-10-27 19:05:53 +0200

    end

--- js/main.js
+++ js/main.js
@@ -1,16 +1,9 @@
-let nome = document.getElementById("nome").value;
-let kilometri = document.getElementById("kilometri").value;
+let nome = document.getElementById("nome");
+let kilometri = document.getElementById("kilometri");
 let genera = document.getElementById("genera");
-
-// let minorenne = document.getElementById("minorenne");
-// let standard = document.getElementById("standard");
-// let senior = document.getElementById("senior");
-
-let eta = document.getElementById("eta").value;
-
+let eta = document.getElementById("eta");
 let prezzoPerKm = 0.21
-let km =parseInt(kilometri);
-let prezzoTotale = (km * prezzoPerKm);
+let annulla = document.getElementById("annulla");
 
 
 
@@ -21,28 +14,66 @@ let prezzoTotale = (km * prezzoPerKm);
 
     
     genera.addEventListener("click", function(){
-        
-    (prezzoTotale *= 0.6);
-    console.log("prezzo totale dopo = ", prezzoTotale);
-    console.log("nome dopo prima = ", nome);
-    console.log("eta dopo prima = ", eta);  
-    })
+    let km =parseInt(kilometri.value);
+    let prezzoTotale = (km * prezzoPerKm);
+    let etaValore = eta.value;
     
-    
-    if (eta == "senior") {
+    console.log("nome a = ", nome.value);
+    console.log("eta = ", etaValore);  
+   
+    if (etaValore == "senior") {
         (prezzoTotale *= 0.6);
+        console.log("prezzo totale if = ", prezzoTotale);
+        document.getElementById("costo").innerHTML = prezzoTotale.toFixed(2);
+        document.getElementById("codice").innerHTML = "933223";
+        document.getElementById("carrozza").innerHTML = "9";
+        document.getElementById("offerta").innerHTML = "Biglietto " + etaValore;
+        document.getElementById("nomePasseggero").innerHTML = nome.value;
+
 
     }
-
-else if (eta == "minorenne") {
+    
+    
+    else if (etaValore == "minorenne") {
 
     (prezzoTotale *= 0.8);
+    console.log("prezzo totale 2if = ", prezzoTotale);
+    document.getElementById("costo").innerHTML = prezzoTotale.toFixed(2);
+    document.getElementById("codice").innerHTML = "994443";
+    document.getElementById("carrozza").innerHTML = "6";
+    document.getElementById("offerta").innerHTML = "Biglietto " + etaValore;
+    document.getElementById("nomePasseggero").innerHTML = nome.value;
 }
     
 
-else if (eta == "standard") {
+else if (etaValore == "standard") {
 
     (prezzoPerKm * km) ;
+    console.log("prezzo totale 3if = ", prezzoTotale);
+    document.getElementById("costo").innerHTML = prezzoTotale.toFixed(2);
+    document.getElementById("codice").innerHTML = "992223";
+    document.getElementById("carrozza").innerHTML = "5";
+    document.getElementById("offerta").innerHTML = "Biglietto " + etaValore;
+    document.getElementById("nomePasseggero").innerHTML = nome.value;
 }
+
+
+})
+    
+  annulla.addEventListener("click", function(){
+    if (kilometri.value !=null || nome.value !=null) {
+        kilometri.value = null;
+        nome.value =null;
+        document.getElementById("costo").innerHTML = null;
+        document.getElementById("codice").innerHTML = null;
+        document.getElementById("carrozza").innerHTML = null;
+        document.getElementById("offerta").innerHTML =  null;
+        document.getElementById("nomePasseggero").innerHTML = null;
+    }
+       
+})
+    
+
+
 
    
